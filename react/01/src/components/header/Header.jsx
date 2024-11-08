9dccb7f734e26658f6f9f18c784e4acaade7c69a
Guilherme Padial <guilherme.padial4@gmail.com>
2024-11-08 12:01:02 -0300

    Creation 01

---
+++ react/01/src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import "./header.scss";
+
+export function Header() {
+  return (
+    <>
+      <div className="container__header">
+        <p>Iginite Feed</p>
+      </div>
+    </>
+  );
+}
---
+++ react/01/src/pages/App.jsx
@@ -0,0 +1,10 @@
+import { Post } from "../components/post/Post";
+import { Header } from "../components/header/header";
+
+export function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
