950b7bd7f76c293b4f293c54db988ed72e97e7be
Guilherme Padial <guilherme.padial4@gmail.com>
2024-11-11 11:21:51 -0300

    Update the project

--- react/01/src/components/header/Header.jsx
+++ react/01/src/components/header/Header.jsx
@@ -1,9 +1,12 @@
 import "./header.scss";
 
+import igniteLogo from "../../assets/ignite-simbol.svg";
+
 export function Header() {
   return (
     <>
       <div className="container__header">
+        <img src={igniteLogo} alt="Logotipo do Ignite" />
         <p>Iginite Feed</p>
       </div>
     </>
--- react/01/src/pages/App.jsx
+++ react/01/src/pages/App.jsx
@@ -1,12 +1,22 @@
 import { Header } from "../components/header/header";
+import { Sidebar } from "../components/sidebar/sidebar";
 
-import "../styles/reset.scss"
+import "../styles/reset.scss";
 import "../styles/global.scss";
+import "../styles/app.scss";
 
 export function App() {
   return (
     <>
       <Header />
+
+      <div className="wrapper">
+        <Sidebar />
+
+        <main>
+          <p>test</p>
+        </main>
+      </div>
     </>
   );
 }
