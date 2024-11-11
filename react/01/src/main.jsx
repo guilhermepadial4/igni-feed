7f03361da243b96260fd8d84ce94b976ba11cdef
Guilherme Padial <guilherme.padial4@gmail.com>
2024-11-11 09:31:17 -0300

    Add: global.scss and Update: App.jsx and main.jsx

--- react/01/src/main.jsx
+++ react/01/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./pages/App";
-import "./styles/reset.scss";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
--- react/01/src/pages/App.jsx
+++ react/01/src/pages/App.jsx
@@ -1,5 +1,7 @@
-import { Post } from "../components/post/Post";
 import { Header } from "../components/header/header";
+
+import "../styles/reset.scss"
+import "../styles/global.scss";
 
 export function App() {
   return (
