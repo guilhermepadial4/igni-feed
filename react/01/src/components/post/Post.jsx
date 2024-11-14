efc6168ab6af758a84a1f57954bffbbb35b9853a
Guilherme Padial <guilherme.padial4@gmail.com>
2024-11-14 10:02:28 -0300

    Update the project

--- react/01/src/components/post/Post.jsx
+++ react/01/src/components/post/Post.jsx
@@ -1,56 +1,61 @@
+import { format, formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+
 import { Avatar } from "../avatar/Avatar";
 import { Comment } from "../comment/Comment";
 import "./post.scss";
 
-export function Post() {
+export function Post({ author, publishedAt, content }) {
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR,
+  });
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  });
+
   return (
-    <>
-      <article className="post">
-        <header>
-          <div className="author">
-            <Avatar src="https://github.com/guilhermepadial4.png" />
+    <article className="post">
+      <header>
+        <div className="author">
+          <Avatar src={author.avatarUrl} />
 
-            <div className="author__info">
-              <strong>Guilherme Padial</strong>
-              <span>Web Developer</span>
-            </div>
+          <div className="author__info">
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
-
-          <time title="12 de novembro às 8:53h" dateTime="2024-11-12 08:53:26">
-            Públicado há 1h
-          </time>
-        </header>
-
-        <div className="content">
-          <p>Fala galeraa 👋</p>
-          <p>
-            Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é
-            DoctorCare 🚀
-          </p>
-          <p>
-            👉 <a href="#">jane.design/doctorcare</a>
-          </p>
-
-          <p>
-            <a href="#">#novoprojeto</a> <a href="#">#nlw</a> <a href="#">#rocketseat</a>
-          </p>
         </div>
 
-        <form className="comment__form">
-          <strong>Deixe seu feedback</strong>
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
+        </time>
+      </header>
 
-          <textarea placeholder="Deixe um comentário" />
+      <div className="content">
+        {content.map((line) => {
+          if (line.type === "paragraph") {
+            return <p>{line.content}</p>;
+          } else if (line.type === "link") {
+            return <p><a>{line.content}</a></p>;
+          }
+        })}
+      </div>
 
-          <footer>
-            <button type="submit">Publicar</button>
-          </footer>
-        </form>
+      <form className="comment__form">
+        <strong>Deixe seu feedback</strong>
 
-        <div className="comment__list">
-          <Comment />
-          <Comment />
-        </div>
-      </article>
-    </>
+        <textarea placeholder="Deixe um comentário" />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+
+      <div className="comment__list">
+        <Comment />
+        <Comment />
+      </div>
+    </article>
   );
 }
--- react/01/src/pages/App.jsx
+++ react/01/src/pages/App.jsx
@@ -6,6 +6,27 @@ import "../styles/reset.scss";
 import "../styles/global.scss";
 import "../styles/app.scss";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/guilhermepadial4.png",
+      name: "Guilherme Padial",
+      role: "Web Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2024-11-14 8:00:00"),
+  },
+];
+
 export function App() {
   return (
     <>
@@ -15,8 +36,15 @@ export function App() {
         <Sidebar />
 
         <main>
-          <Post />
-          <Post />
+          {posts.map((post) => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </>
