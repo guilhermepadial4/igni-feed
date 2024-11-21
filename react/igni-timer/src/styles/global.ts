b92a49151b95a595c7c39e1166e25cbd9d5f0440
Guilherme Padial <guilherme.padial4@gmail.com>
2024-11-21 14:02:45 -0300

    add: router and creation header

--- react/igni-timer/src/styles/global.ts
+++ react/igni-timer/src/styles/global.ts
@@ -19,7 +19,7 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   body, input-security, textarea, button {
-    font-family: "Roboto" sans-serif;
+    font-family: "Roboto", sans-serif;
     font-weight: 400;
     font-size: 1rem;
   }
