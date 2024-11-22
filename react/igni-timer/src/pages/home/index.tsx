a9acc32eb4611c7d233d3502f4a2469db215c201
Guilherme Padial <guilherme.padial4@gmail.com>
2024-11-22 13:15:09 -0300

    add: new functions in inputs from home, and add webkit from InputTask

--- react/igni-timer/src/pages/home/index.tsx
+++ react/igni-timer/src/pages/home/index.tsx
@@ -8,10 +8,17 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput type="text" id="task" placeholder="Dê um nome para o seu projeto" />
+          <TaskInput type="text" list="task-suggestions" id="task" placeholder="Dê um nome para o seu projeto" />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Banana" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
-          <MinutesAmountInput type="number" id="minutesAmount" placeholder="00" />
+          <MinutesAmountInput type="number" id="minutesAmount" step={5} min={5} max={60} placeholder="00" />
 
           <span>minutos.</span>
         </FormContainer>
