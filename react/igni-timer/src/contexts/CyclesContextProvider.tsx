ef990fb3c38a96fbca27b488547d4f58c10313af
Guilherme Padial <guilherme.padial4@gmail.com>
2024-12-05 14:32:26 -0300

    add: storage in cycles context provider. fix: some bugs in countdown

--- react/igni-timer/src/contexts/CyclesContextProvider.tsx
+++ react/igni-timer/src/contexts/CyclesContextProvider.tsx
@@ -1,8 +1,9 @@
-import { ReactNode, useState, useReducer } from "react";
+import { ReactNode, useState, useReducer, useEffect } from "react";
 
 import { CyclesContext } from "./CyclesContext";
 import { Cycle, cyclesReducer } from "../reducers/cycles/reducer";
 import { addNewCycleAction, InterreputCurrentCycleAction, markCurrentCycleAsFinishedAction } from "../reducers/cycles/actions";
+import { differenceInSeconds } from "date-fns";
 
 interface CreateCycleData {
   task: string;
@@ -14,16 +15,40 @@ interface CyclesContextProviderProps {
 }
 
 export const CyclesContextProvider = ({ children }: CyclesContextProviderProps) => {
-  const [cyclesState, dispatch] = useReducer(cyclesReducer, {
-    cycles: [],
-    activeCycleId: null,
-  });
+  const [cyclesState, dispatch] = useReducer(
+    cyclesReducer,
+    {
+      cycles: [],
+      activeCycleId: null,
+    },
+    (initialState) => {
+      const storedStateAsJSON = localStorage.getItem("@igni-timer: cycles-state-1.0.0");
 
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
+      if (storedStateAsJSON) {
+        return JSON.parse(storedStateAsJSON);
+      }
+
+      return initialState;
+    }
+  );
 
   const { cycles, activeCycleId } = cyclesState;
 
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+    if (activeCycle) {
+      return differenceInSeconds(new Date(), new Date(activeCycle.startDate));
+    }
+
+    return 0;
+  });
+
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cyclesState);
+
+    localStorage.setItem("@igni-timer: cycles-state-1.0.0", stateJSON);
+  }, [cyclesState]);
 
   const setSecondsPassed = (seconds: number) => {
     setAmountSecondsPassed(seconds);
