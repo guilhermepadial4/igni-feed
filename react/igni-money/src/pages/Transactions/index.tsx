e938fadffd716b6f9e28aa89d2e737e739893f6c
Guilherme Padial <guilherme.padial4@gmail.com>
2025-01-07 14:48:49 -0300

    add: creation consume api

--- react/igni-money/src/pages/Transactions/index.tsx
+++ react/igni-money/src/pages/Transactions/index.tsx
@@ -1,10 +1,33 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 
 import { TransactionsContainer, TransactionsTable, PriceHighLight } from "./styles";
 
+interface Transactions {
+  id: number;
+  description: string;
+  type: "income" | "outcome";
+  price: number;
+  category: string;
+  createdAt: string;
+}
+
 export function Transactions() {
+  const [transactions, setTransactions] = useState<Transactions[]>([]);
+
+  async function loadTransactions() {
+    const response = await fetch("http://localhost:3000/transactions");
+    const data = await response.json();
+
+    setTransactions(data);
+  }
+
+  useEffect(() => {
+    loadTransactions();
+  });
+
   return (
     <div>
       <Header />
@@ -14,32 +37,18 @@ export function Transactions() {
         <SearchForm />
         <TransactionsTable>
           <tbody>
-            <tr>
-              <td width="50%">Desenvolvimento de site</td>
-              <td>
-                <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
-              </td>
-              <td>Venda</td>
-              <td>13/11/2024</td>
-            </tr>
-
-            <tr>
-              <td width="50%">Pizza</td>
-              <td>
-                <PriceHighLight variant="outcome">- R$ 58,00</PriceHighLight>
-              </td>
-              <td>Alimentação</td>
-              <td>15/11/2024</td>
-            </tr>
-
-            <tr>
-              <td width="50%">Aluguel ap</td>
-              <td>
-                <PriceHighLight variant="outcome">- R$ 1.200,00</PriceHighLight>
-              </td>
-              <td>Casa</td>
-              <td>05/11/2024</td>
-            </tr>
+            {transactions.map((transaction) => {
+              return (
+                <tr key={transaction.id}>
+                  <td width="50%">{transaction.description}</td>
+                  <td>
+                    <PriceHighLight variant={transaction.type}>{transaction.price}</PriceHighLight>
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>{transaction.createdAt}</td>
+                </tr>
+              );
+            })}
           </tbody>
         </TransactionsTable>
       </TransactionsContainer>
