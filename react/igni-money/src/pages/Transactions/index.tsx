94de6d2ecf3beced50318b7d77c312d830279c81
Guilherme Padial <guilherme.padial4@gmail.com>
2025-01-08 12:00:35 -0300

    add: creation format price and date

--- react/igni-money/src/pages/Transactions/index.tsx
+++ react/igni-money/src/pages/Transactions/index.tsx
@@ -1,10 +1,12 @@
 import { useContext } from "react";
+
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 
 import { TransactionsContainer, TransactionsTable, PriceHighLight } from "./styles";
-import { TransactionsContext } from "../../contexts/TransactionsContext";
 
 export function Transactions() {
   const { transactions } = useContext(TransactionsContext);
@@ -23,10 +25,13 @@ export function Transactions() {
                 <tr key={transaction.id}>
                   <td width="50%">{transaction.description}</td>
                   <td>
-                    <PriceHighLight variant={transaction.type}>{transaction.price}</PriceHighLight>
+                    <PriceHighLight variant={transaction.type}>
+                      {transaction.type === "outcome" && "- "}
+                      {priceFormatter.format(transaction.price)}
+                    </PriceHighLight>
                   </td>
                   <td>{transaction.category}</td>
-                  <td>{transaction.createdAt}</td>
+                  <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
                 </tr>
               );
             })}
