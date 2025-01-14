268fe6c1aa241f2ce08b5cf1ec1d83191f210581
Guilherme Padial <guilherme.padial4@gmail.com>
2025-01-14 12:27:53 -0300

    add: creation project igni-pizza and add shadcn/ui

---
+++ react/igni-pizza/src/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from "./components/ui/button";
+
+export function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Button>Click Me</Button>
+    </div>
+  );
+}
