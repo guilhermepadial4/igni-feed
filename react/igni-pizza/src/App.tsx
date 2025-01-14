89244c59ee899348c3c5605e6c5ad4e1791a6af5
Guilherme Padial <guilherme.padial4@gmail.com>
2025-01-14 15:28:06 -0300

    add: add helmet and update the routes

--- react/igni-pizza/src/App.tsx
+++ react/igni-pizza/src/App.tsx
@@ -1,7 +1,13 @@
+import { Helmet, HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router-dom";
 
 import { router } from "./routes";
 
 export function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | igni.pizza" />
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  );
 }
--- react/igni-pizza/src/pages/app/dashboard.tsx
+++ react/igni-pizza/src/pages/app/dashboard.tsx
@@ -1,3 +1,10 @@
+import { Helmet } from "react-helmet-async";
+
 export function Dashboard() {
-  return <h1>Hello Dashboard</h1>;
+  return (
+    <>
+      <Helmet title="Dashboard" />
+      <h1>Hello Dashboard</h1>
+    </>
+  );
 }
